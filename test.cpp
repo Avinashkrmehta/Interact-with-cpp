0a65a889cc80a4698cd18e4cf89e041f6b17c1a3
AK Mehta <avi4791@gmail.com>
2023-06-15 13:11:24 +0530

    first initial

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int a[] = { 1, -2, 1, 0, 5 };
+  int z = 0;
+  int n = sizeof(a) / sizeof(a[0]);
+  cout <<  sizeof(a) << endl;
+  cout <<  sizeof(a[0]) << endl;
+  cout << n << endl;
+  return 0;
+}
